981230aa98706509bbd119b0d94a441b697e9da3
Daniel <bikov22022006@gmail.com>
2026-02-09 22:51:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,23 +7,22 @@ from disnake.ext import commands, tasks
 from colorlog import ColoredFormatter
 
 load_dotenv()
-handler = logging.StreamHandler()
-handler.setFormatter(ColoredFormatter(
-    "%(log_color)s%(asctime)s [%(levelname)s] %(name)s:%(reset)s %(message)s",
-    datefmt="%H:%M:%S",
-    log_colors={
-        "DEBUG": "cyan",
-        "INFO": "green",
-        "WARNING": "yellow",
-        "ERROR": "red",
-        "CRITICAL": "bold_red",
-    },
-))
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(
+    ColoredFormatter(
+        "%(log_color)s%(asctime)s [%(levelname)s] %(name)s:%(reset)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },
+    )
+)
 
-root = logging.getLogger()
-root.setLevel(logging.INFO)
-root.handlers.clear()
-root.addHandler(handler)
+logging.basicConfig(level=logging.INFO, handlers=[handler], force=True)
 
 bot = commands.InteractionBot(intents=disnake.Intents.all())
 
