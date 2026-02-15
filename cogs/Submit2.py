6dec521c488c0d5c72e93b7e943fce71e63d0d25
Daniel <bikov22022006@gmail.com>
2026-02-16 00:14:16 +0300

    Update Submit2.py

--- cogs/Submit2.py
+++ cogs/Submit2.py
@@ -79,9 +79,7 @@ class Submit2(commands.Cog):
                     DEBUG_DIR = "debug_out"
                     os.makedirs(DEBUG_DIR, exist_ok=True)
                     res = OCR.process_one(path=None, url=url, debug_dir=DEBUG_DIR)
-                    d = json.dumps(res, ensure_ascii=False, indent=2)
-                    print(type(d))
-                    return
+                    d = json.loads(json.dumps(res, ensure_ascii=False, indent=2))
                     month = d["ocr"]["best"]["month"]
                     day = d["ocr"]["best"]["day"]
                     matches = d["ocr"]["best"]["matches"]
@@ -89,7 +87,7 @@ class Submit2(commands.Cog):
 
                     await inter.followup.send(f"Как я могу заметить из вашего изображения, Вы сыграли {matches} матчей <t:{int(date)}:D>.\nПравильно ли сработало распознавание?")
 
-                    
+                    return
                     
                     date = datetime.datetime.strftime(datetime.datetime.now(), "%b.%d.")
                     key_value = inter.author.name
